0f9618f3a9bfa0d42f6dc5ec2ef082bccede2264
Joakim Östelid <joakim.ostelid@outlook.com>
2023-01-24 22:37:27 +0100

    state

--- src/App.tsx
+++ src/App.tsx
@@ -44,9 +44,7 @@ function CartPreview() {
   return (
     <div className="cart-preview">
       <div className="cart-preview__left mr-l">
-        <i className="fa fa-shopping-cart"></i>
-        1
-        {/* {{ cartService.countTotalQuantity() }} */}
+        <i className="fa fa-shopping-cart"></i>1{/* {{ cartService.countTotalQuantity() }} */}
       </div>
       <div className="vl"></div>
       <div className="cart-preview__right">0 kr{/* {{ cartService.getTotalPrice() }} kr */}</div>
