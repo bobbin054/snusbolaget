b22fbdf734e5228078b0a6423021f00b02ade0db
Joakim Östelid <joakim.ostelid@outlook.com>
2023-02-18 22:22:56 +0100

    stae

--- src/components/productsProvider/productsProvider.tsx
+++ src/components/productsProvider/productsProvider.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PRODUCTS } from "../../assets/products";
 import { IProduct } from "../../interfaces/IProduct";
+import axios from "axios";
 
 export interface IProductsContext {
   products: IProduct[];
@@ -9,6 +10,13 @@ export interface IProductsContext {
 export const ProductsContext = React.createContext<IProductsContext>({ products: [], getProduct: () => undefined });
 
 export const ProductsProvider = ({ children }: { children: any }) => {
+  axios({
+    method: "get",
+    url: "https://localhost:7083/Products",
+  }).then((response) => {
+    console.log(response);
+  });
+
   const [products, setProducts] = React.useState<IProduct[]>(PRODUCTS);
   const getProduct = (name: string) => {
     return products.find((product) => product.name === name);
