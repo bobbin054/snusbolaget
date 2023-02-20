44170b571d6131479b1d54510e2e7297f8ac081f
Joakim Östelid <joakim.ostelid@outlook.com>
2023-02-20 21:48:01 +0100

    state

--- api/Models/ProductForUpdateDto.cs
+++ api/Models/ProductForUpdateDto.cs
@@ -5,8 +5,19 @@ namespace api.Models
 
     public class ProductForUpdateDto
     {
-        [MaxLength(200)]
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string NicotineAmount { get; set; }
         public string Description { get; set; }
+        public double Price { get; set; }
+        public double StarRating { get; set; }
+        public string ImageUrl { get; set; }
+        public string NicotineLevel { get; set; }
+        public string Taste { get; set; }
+        public string ContentWeight { get; set; }
+        public string ContentWeightUnit { get; set; }
+        public string Producer { get; set; }
 
     }
 
