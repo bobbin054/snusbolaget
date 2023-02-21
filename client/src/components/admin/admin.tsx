eb666a2839473284ce3c077b134ee7e6432ffa43
Joakim Östelid <joakim.ostelid@outlook.com>
2023-02-21 15:59:07 +0100

    state

--- client/src/components/admin/admin.tsx
+++ client/src/components/admin/admin.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { PRODUCTS_ENDPOINT, useProducts } from "../../hooks/useProducts";
 import { IProduct } from "../../interfaces/IProduct";
 import axios from "axios";
+import styles from "./admin.module.scss";
 
 export default () => {
   const { products } = useProducts();
@@ -10,15 +11,56 @@ export default () => {
   return (
     <>
       {products?.map((product) => (
-        <ProductForm key={product.id} product={product} />
+        <UpdateProductForm key={product.id} product={product} />
       ))}
     </>
   );
 };
 
-const ProductForm = ({ product }: { product: IProduct }) => {
+const UpdateProductForm = ({ product }: { product: IProduct }) => {
   const [pendingProduct, setPendingProduct] = React.useState(product);
   const descId = React.useId();
+  const priceId = React.useId();
+  const imageUrlId = React.useId();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const result = await axios.put(`${PRODUCTS_ENDPOINT}/${product.id}`, pendingProduct);
+    console.log(result);
+  };
+  return (
+    <form className={styles.formColumn} onSubmit={handleSubmit}>
+      <label htmlFor={descId}>Description</label>
+      <input
+        id={descId}
+        type="text"
+        value={pendingProduct.description}
+        onChange={(e) => setPendingProduct({ ...pendingProduct, description: e.target.value })}
+      />
+      <label htmlFor={priceId}>Price</label>
+      <input
+        id={priceId}
+        type="number"
+        value={pendingProduct.price}
+        onChange={(e) => setPendingProduct({ ...pendingProduct, price: Number(e.target.value) })}
+      />
+      <label htmlFor={imageUrlId}>Image URL</label>
+      <input
+        id={imageUrlId}
+        type="url"
+        value={pendingProduct.imageUrl}
+        onChange={(e) => setPendingProduct({ ...pendingProduct, imageUrl: e.target.value })}
+      />
+      Image preview:
+      <img className={styles.imagePreview} src={pendingProduct.imageUrl} alt={pendingProduct.name} />
+      <button type="submit">Save</button>
+    </form>
+  );
+};
+
+const PatchField = ({ product }: { product: IProduct }) => {
+  const [pendingProduct, setPendingProduct] = React.useState(product);
+  const patchDescId = React.useId();
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const result = await axios.patch(`${PRODUCTS_ENDPOINT}/${product.id}`, [
@@ -35,8 +77,8 @@ const ProductForm = ({ product }: { product: IProduct }) => {
   };
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor={descId}>Product name</label>
-      <input id={descId} type="text" value={pendingProduct.description} onChange={handleChange} />
+      <label htmlFor={patchDescId}>Description</label>
+      <input id={patchDescId} type="text" value={pendingProduct.description} onChange={handleChange} />
       <button type="submit">Save</button>
     </form>
   );
