8d2d753127469b8ea83de0aaad8c30a9b64cdf59
Joakim Östelid <joakim.ostelid@outlook.com>
2023-03-17 15:23:36 +0100

    supabase init

--- client/src/hooks/useProducts.tsx
+++ client/src/hooks/useProducts.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { IProduct } from "../interfaces/IProduct";
 import axios from "axios";
 import useSWR, { mutate } from "swr";
+import { createClient } from "@supabase/supabase-js";
 
 type STATUS = "idle" | "loading" | "error" | "success";
-export const PRODUCTS_ENDPOINT = "https://localhost:7083/Products";
-async function fetcher(url: string): Promise<IProduct[]> {
+export const PRODUCTS_ENDPOINT = "https://ivfguobvnkblumlcqcod.supabase.co";
+const supabaseUrl = 'https://ivfguobvnkblumlcqcod.supabase.co';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2Zmd1b2J2bmtibHVtbGNxY29kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNzU1NjksImV4cCI6MTk5Mzk1MTU2OX0.I0QRDquizwDLkbN6kY_Vp-Ro-p0-Xg17Yh18KzpOSu4";
+const supabase = createClient(supabaseUrl, supabaseKey)
+async function fetcher_old(url: string): Promise<IProduct[]> {
   try {
     const response = await axios.get<IProduct[]>(url);
     return response.data;
@@ -15,8 +19,15 @@ async function fetcher(url: string): Promise<IProduct[]> {
   }
 }
 
+async function fetcher() {
+  const { data } = await supabase.from("Products").select();
+  console.log("data: ", data);
+  return data;
+}
+
 export const useProducts = () => {
   console.log("useProducts");
+ 
   const { data: products, mutate, isLoading } = useSWR<IProduct[]>(PRODUCTS_ENDPOINT, fetcher);
   const getProduct = (name: string) => {
     return products?.find((product) => product.name === name);
