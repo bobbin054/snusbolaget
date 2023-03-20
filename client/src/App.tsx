17aec38e4aaf90efdb293f58d9c17597e3441d66
Joakim Östelid <joakim.ostelid@outlook.com>
2023-03-20 21:35:42 +0100

    state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,32 @@
-import { Nav } from "./components/nav/nav";
-import { CartProvider } from "./components/cartProvider/cartProvider";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { ProductList } from "./components/productList/productList";
-import { Cart } from "./components/cart/cart";
-import { ProductDetails } from "./components/productDetails/productDetails";
-import "font-awesome/css/font-awesome.min.css";
-import "./app.scss";
-import Admin from "./components/admin/admin";
-import Login from "./components/login/login";
+import { Nav } from './components/nav/nav';
+import { CartProvider } from './components/cartProvider/cartProvider';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { ProductList } from './components/productList/productList';
+import { Cart } from './components/cart/cart';
+import { ProductDetails } from './components/productDetails/productDetails';
+import 'font-awesome/css/font-awesome.min.css';
+import './app.scss';
+import Admin from './components/admin/admin';
+import Login from './components/login/login';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <CartProvider>
-        <Routes>
-          <Route path="/" element={<Nav />}>
-              <Route path="/" element={<div>Snus is tasty!!!</div>} />
-              <Route path="products">
-                <Route index element={<ProductList />} />
-                <Route path=":name" element={<ProductDetails />} />
-              </Route>
-              <Route path="login" element={<Login/>} />
-              <Route path="admin" element={<Admin />} />
-              <Route path="cart" element={<Cart />} />
-              <Route path="*" element={<div>You shall not pass!</div>} />
-          </Route>
-        </Routes>
-      </CartProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <CartProvider>
+                <Routes>
+                    <Route path="/" element={<Nav />}>
+                        <Route path="/" element={<div>Snus is tasty!!!</div>} />
+                        <Route path="products">
+                            <Route index element={<ProductList />} />
+                            <Route path=":name" element={<ProductDetails />} />
+                        </Route>
+                        <Route path="login" element={<Login/>} />
+                        <Route path="admin" element={<Admin />} />
+                        <Route path="cart" element={<Cart />} />
+                        <Route path="*" element={<div>You shall not pass!</div>} />
+                    </Route>
+                </Routes>
+            </CartProvider>
+        </BrowserRouter>
+    );
 }
--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -1,26 +1,26 @@
-import { useState, useEffect } from 'react'
-import Auth from '../auth/auth'
-import Account from '../account/account'
-import { supabase } from '../../supabaseClient/supabaseClient'
+import { useState, useEffect } from "react";
+import Auth from "../auth/auth";
+import Account from "../account/account";
+import { supabase } from "../../supabaseClient/supabaseClient";
 
 function Login() {
-  const [session, setSession] = useState(null)
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+      setSession(session);
+    });
+  }, []);
 
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
       {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
