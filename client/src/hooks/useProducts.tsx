958ebf923c96cf94cc57052298a5d972a5a3cf33
Joakim Östelid <joakim.ostelid@outlook.com>
2023-03-20 11:48:19 +0100

    state

--- client/src/hooks/useProducts.tsx
+++ client/src/hooks/useProducts.tsx
@@ -1,16 +1,13 @@
-import React from "react";
 import { IProduct } from "../interfaces/IProduct";
-import axios from "axios";
-import useSWR, { mutate } from "swr";
+import useSWR from "swr";
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./products";
 
-type STATUS = "idle" | "loading" | "error" | "success";
 export const PRODUCTS_ENDPOINT = "https://ivfguobvnkblumlcqcod.supabase.co";
-const supabaseUrl = 'https://ivfguobvnkblumlcqcod.supabase.co';
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2Zmd1b2J2bmtibHVtbGNxY29kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNzU1NjksImV4cCI6MTk5Mzk1MTU2OX0.I0QRDquizwDLkbN6kY_Vp-Ro-p0-Xg17Yh18KzpOSu4";
-const supabase = createClient<Database>(supabaseUrl, supabaseKey)
-
+const supabaseUrl = "https://ivfguobvnkblumlcqcod.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2Zmd1b2J2bmtibHVtbGNxY29kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNzU1NjksImV4cCI6MTk5Mzk1MTU2OX0.I0QRDquizwDLkbN6kY_Vp-Ro-p0-Xg17Yh18KzpOSu4";
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 async function fetcher() {
   const { data } = await supabase.from("Products").select("*");
@@ -20,7 +17,7 @@ async function fetcher() {
 
 export const useProducts = () => {
   console.log("useProducts");
- 
+
   const { data: products, mutate, isLoading } = useSWR<IProduct[]>(PRODUCTS_ENDPOINT, fetcher);
   const getProduct = (name: string) => {
     return products?.find((product) => product.name === name);
