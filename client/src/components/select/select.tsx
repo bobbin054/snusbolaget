70af70fc8e424a92743b61d72002d7dba63e1c13
Joakim Östelid <joakim.ostelid@outlook.com>
2023-06-01 21:14:02 +0200

    state

--- client/src/components/select/select.tsx
+++ client/src/components/select/select.tsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 export interface IOptions {
   id: number;
   label: string;
@@ -13,23 +15,27 @@ export interface ISelectProps {
   className?: string;
 }
 
-export default function Select({ label, options, selected, setSelected,className }: ISelectProps) {
-    return (
-        <>
-            {label && <label htmlFor="quantity">Quantity</label>}
-            <select
-                className={className}
-                title="quantity"
-                name="quantity"
-                value={selected.id}
-                onChange={(e) => setSelected(options[+e.target.value - 1])}
-            >
-                {options.map((option) => (
-                    <option key={option.id} value={option.id} disabled={!selected.enabled}>
-                        {option.label}
-                    </option>
-                ))}
-            </select>
-        </>
-    );
+const StyledSelect = styled.select`
+  height: max-content;
+`;
+
+export default function Select({ label, options, selected, setSelected, className }: ISelectProps) {
+  return (
+    <>
+      {label && <label htmlFor="quantity">Quantity</label>}
+      <StyledSelect
+        className={className}
+        title="quantity"
+        name="quantity"
+        value={selected.id}
+        onChange={(e) => setSelected(options[+e.target.value - 1])}
+      >
+        {options.map((option) => (
+          <option key={option.id} value={option.id} disabled={!selected.enabled}>
+            {option.label}
+          </option>
+        ))}
+      </StyledSelect>
+    </>
+  );
 }
