66eb0ea7a9a3af2be8295cc4a316818f8ca7482b
Joakim Östelid <joakim.ostelid@outlook.com>
2023-06-07 21:16:40 +0200

    state

--- snusbolaget/src/components/productForm.tsx
+++ snusbolaget/src/components/productForm.tsx
@@ -1,15 +1,5 @@
-import React from "react";
-import styled from "styled-components";
-
 const ProductForm = ({ ...props }) => {
-  const StyledForm = styled.form`
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 10px;
-    border: 1px solid #ccc;
-    padding: 10px;
-  `;
-  return <StyledForm {...props} className="flex flex-col mb-10 border border-gray-300 p-10" />;
+  return <form {...props} className="flex flex-col mb-10 border border-gray-300 p-10" />;
 };
 
 export default ProductForm;
